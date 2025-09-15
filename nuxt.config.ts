f4cf332d97e901a306f4f60b3893cd3c274d080b
Maystrenko Ilya <visall1@yandex.ru>
2025-09-16 00:15:24 +0300

    :sparkles: setup eslint / lint-staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint"],
+  eslint: {
+    config: {
+      standalone: false, // <---
+    },
+  },
+});
