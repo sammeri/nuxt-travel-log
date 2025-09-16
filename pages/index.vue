5301253639e72e9fb7387927d514ebd7c76fd1e3
Maystrenko Ilya <visall1@yandex.ru>
2025-09-16 23:34:38 +0300

    :sparkles: add landing page

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Hello there
+        </h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+          quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
