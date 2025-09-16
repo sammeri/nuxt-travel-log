27880bc618379e8397a813b63cc11b94e159c9e8
Maystrenko Ilya <visall1@yandex.ru>
2025-09-16 18:46:11 +0300

    :spearkles: setup tailwind / daisy ui / icons

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-3xl font-bold underline">
+    <h1>Hellp</h1>
+    <button class="btn btn-secondary">
+      <Icon name="tabler:map" />
+      View Map
+    </button>
+  </div>
+</template>
--- app/app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <h1>Hellp</h1>
-  </div>
-</template>
