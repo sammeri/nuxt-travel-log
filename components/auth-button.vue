96926db10db57702247efef201493d0fa763cde4
Maystrenko Ilya <visall1@yandex.ru>
2025-09-24 18:59:21 +0300

    💜 add github oauth / turso db settings / 🍍add pinia

---
+++ components/auth-button.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Войти
+    <span v-if="authStore.loading" class="loading loading-bars loading-lg"></span>
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="24"
+    />
+  </button>
+</template>
+
+<style>
+
+</style>
---
+++ stores/auth.ts
@@ -0,0 +1,23 @@
+import { createAuthClient } from "better-auth/client";
+// import { defineStore } from "pinia";
+
+const authClient = createAuthClient();
+
+export const useAuthStore = defineStore("useAuthStore", () => {
+  const loading = ref(false);
+
+  async function signIn() {
+    loading.value = true;
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/dashboard",
+      errorCallbackURL: "/error",
+    });
+    loading.value = false;
+  }
+
+  return {
+    loading,
+    signIn,
+  };
+});
